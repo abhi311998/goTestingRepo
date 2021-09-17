a4553054bfeb563a0689d3e1921398ff0dd44b16
Abhishek Keshari @tanla <abhishek.keshari@tanla.com>
2021-09-17 13:43:03 +0530

    Edited tests

--- utils/sort/sort_test.go
+++ utils/sort/sort_test.go
@@ -7,7 +7,8 @@ import (
 
 func TestBubbleSort(t *testing.T) {
 	// Init
-	arr := []int{9, 7, 5, 3, 1, 2, 4, 6, 8, 0}
+	// arr := []int{9, 7, 5, 3, 1, 2, 4, 6, 8, 0}
+	arr := []int{9, 8, 7, 6, 5, 4, 3, 2, 1, 0}
 	fmt.Println("Before:", arr)
 
 	// Execution
@@ -15,10 +16,10 @@ func TestBubbleSort(t *testing.T) {
 	fmt.Println("After:", arr)
 
 	// Validation
-	// if arr[0] != 9 {
-	// 	t.Error("Expected", 9, " Got", arr[0], " at index 0")
-	// }
-	// if arr[len(arr)-1] != 1 {
-	// 	t.Error("Expected", 9, " Got", arr[0], " at last index")
-	// }
+	if arr[0] != 9 {
+		t.Error("Expected", 9, " Got", arr[0], " at index 0")
+	}
+	if arr[len(arr)-1] != 0 {
+		t.Error("Expected", 0, " Got", arr[len(arr)-1], " at last index")
+	}
 }
